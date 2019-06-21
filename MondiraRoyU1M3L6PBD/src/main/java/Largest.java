6b43b732778f2f93a90ed2e289dc8f0f831bf944
mondira-roy <mondira.mukherjee.roy@gmail.com>
2019-06-20 21:27:24 -0400

    U1M3L6PBD

---
+++ MondiraRoyU1M3L6PBD/src/main/java/Largest.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int num = rand.nextInt(100) + 1;
+            list.add(num);
+        }
+            System.out.println(list);
+
+            int max = 0;
+            for (Integer i1 : list) {
+                // update max if found to be less than
+                // the current element
+                if (max < i1) {
+                    max = i1;
+                }
+
+            }
+            System.out.println("The largest value is " + max);
+        }
+
+    }
