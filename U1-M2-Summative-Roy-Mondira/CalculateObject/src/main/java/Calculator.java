54eba76d6ff086a7912750a27122fd82644ba8e3
mondira-roy <51178938+mondira-roy@users.noreply.github.com>
2019-06-27 11:26:26 -0400

    Update Calculator.java

--- U1-M2-Summative-Roy-Mondira/CalculateObject/src/main/java/Calculator.java
+++ U1-M2-Summative-Roy-Mondira/CalculateObject/src/main/java/Calculator.java
@@ -1,34 +1,34 @@
 public class Calculator {
 
     public void add(int a ,int b){
-        System.out.println(a+" + "+ b +" = "+ (a+b));
+        System.out.println(a+" + "+ b +" = "+ (a + b));
 
 
     }
     public void subtract(int a ,int b){
-        System.out.println(a+" - "+b +" = "+ (a-b));
+        System.out.println(a+" - "+b +" = "+ (a - b));
 
     }
     public void Multiply(int a ,int b){
-        System.out.println(a+" * "+b+" = "+ (a+b));
+        System.out.println(a+" * "+b+" = "+ (a * b));
 
     }
 
     public void divide(int a ,int b){
-        System.out.println(a + " / " + b +" = "+ (a+b));
+        System.out.println(a + " / " + b +" = "+ (a / b));
 
     }
     public void add(double a ,double b){
-        System.out.println(a+" + "+b+" = "+ (a+b));
+        System.out.println(a+" + "+b+" = "+ (a + b));
 
 
     }
     public void subtract(double a , double b){
-        System.out.println(a+" - "+b +" = "+ (a-b));
+        System.out.println(a+" - "+b +" = "+ (a - b));
 
     }
     public void multiply(double a ,double b){
-        System.out.println(a+" * "+b +" = "+ (a*b));
+        System.out.println(a+" * "+b +" = "+ (a * b));
 
     }
 
