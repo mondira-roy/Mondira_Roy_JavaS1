19a3af8b6a7eb5f1e88263ddfa6c2a74ec9aa353
mondira-roy <51178938+mondira-roy@users.noreply.github.com>
2019-07-09 02:28:12 -0400

    Add files via upload

---
+++ U1-M4-Summative-Roy-Mondira/src/main/java/com/company/model/WordService.java
@@ -0,0 +1,24 @@
+package com.company.model;
+
+
+
+public class WordService {
+    private String word;
+    private String definition;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+}
