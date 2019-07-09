ae3cf5b55193f16ab8e97bd7f1c5b99722c124e0
mondira-roy <mondira.mukherjee.roy@gmail.com>
2019-07-08 22:35:28 -0400

    U1-M4-Summative_Roy-Mondira

---
+++ U1-M4-Summative-Roy-Mondira/U1-M4-Summative-Roy-Mondira/src/main/java/com/company/QuoteWordAndMagic8BallApplication.java
@@ -0,0 +1,11 @@
+package com.company;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteWordAndMagic8BallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuoteWordAndMagic8BallApplication.class, args);
+        }
+}
+
